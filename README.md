a03bff6fd72aac11d00601031dda75ff49d39caf
Mohammadalsmadi2000 <mohammadalsmadi.fcbayern@gmail.com>
2023-02-23 21:59:53 +0000

    done

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 
 
-# Movies-Library - 1.0.0
+# Movies-Library - 1.1
 
 **Author Name**: Mohammad Alsmadi
 
 ## WRRC
-![WRRC](https://user-images.githubusercontent.com/60603704/220379488-3493df49-fe3f-40b6-97df-bdb366876beb.jpg)
-
+![WRRC](https://user-images.githubusercontent.com/60603704/221033363-3f381f30-3a80-42b7-ba4f-0e5bac7aaa90.jpg)
 ## Overview
 The movie app is a web application that allows users to check the latest movies based on categories. It provides a user-friendly interface to browse movies and filter them based on genre, release year, and other criteria. The app is built using Node.js and Express, and it utilizes a database to store movie data.
 ## Getting Started
@@ -22,3 +21,6 @@ To build and run the movie app on your own machine, follow these steps:
 5. Run npm start to start the server and launch the app on your local machine.
 ## Project Features
 ("regarding of today lab")Send requests and receive response
+
+### regarding of today lab 12
+1. Request an API from website "TMDB"
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 'use strict';
+// http://localhost:3000/search
 
 
 const express = require('express');
@@ -8,9 +9,13 @@ const cors = require('cors');
 
 const server = express();
 const Data = require('./ Movie Data/data.json');
+const { default: axios } = require('axios');
+require('dotenv').config();
 
 
 server.use(cors());
+server.use(errorHandler)
+
 
 const PORT = 3000;
 
@@ -21,33 +26,105 @@ function Movie(title, poster_path, overview) {
     this.overview = overview;
 }
 
+function MovieInfoFromApi(id, title, release_date, poster_path, overview) {
+    this.id = id;
+    this.title = title;
+    this.release_date = release_date;
+    this.poster_path = poster_path;
+    this.overview = overview;
+}
 //Routes
 
-server.get('/', (req, res) => {
+server.get('/', homeHandler)
+server.get('/favorite', favoriteHandler)
+server.get('/trending', trendingHandler);
+server.get('/search', searchHandler);
+server.get('/review', reviewHandler);
+server.get('/TV', tvHandler);
+server.get('*', defaultHandler)
 
+
+function homeHandler(req, res) {
     const formattedMovies = new Movie(Data.title, Data.poster_path, Data.overview)
     res.status(200).json(formattedMovies);
-})
+}
 
-server.get('/favorite', (req, res) => {
+function favoriteHandler(req, res) {
     res.send('Welcome to Favorite Page')
-})
+}
 
-server.get('/error', (req, res) => {
-    const temp = {
-        status: 500,
-        responseText: "Sorry, something went wrong"
-    }
+function trendingHandler(req, res) {
+    const ApiKey = process.env.APIKey
+    const url = `https://api.themoviedb.org/3/trending/all/week?api_key=${ApiKey}&language=en-US`
+    axios.get(url).then((result) => {
 
-    res.status(500).json(temp)
-})
-server.get('*', (req, res) => {
+        let data = result.data.results;
+
+        let sendInfo = data.map((val) => {
+            return new MovieInfoFromApi(val.id, val.title || val.name, val.release_date || val.first_air_date, val.poster_path, val.overview)
+        })
+
+        res.json(sendInfo)
+    }).catch((err) => {
+        res.send(err);
+    })
+}
+function searchHandler(req, res) {
+    let ApiKey = process.env.APIKey
+    // console.log(ApiKey)
+    let searchWord = 'Harry%20Potter'
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&language=en-US&query=${searchWord}&page=2`
+    axios.get(url).then((result) => {
+
+        let data = result.data.results;
+
+        let sendInfo = data.map((val) => {
+            return new MovieInfoFromApi(val.id, val.title || val.name, val.release_date || val.first_air_date, val.poster_path, val.overview)
+        })
+
+        res.json(sendInfo)
+    }).catch((err) => {
+        res.send(err);
+    })
+}
+
+function reviewHandler(req, res) {
+    const idReview = process.env.idReview;
+    const ApiKey = process.env.APIKey
+    const url = `https:api.themoviedb.org/3/review/${idReview}?api_key=${ApiKey}`
+    axios.get(url).then((result) => {
+        res.json(result.data)
+    }).catch((err) => {
+        res.send(err);
+    })
+
+}
+function tvHandler(req, res) {
+    const id = '9813'
+    const ApiKey = process.env.APIKey
+    const url = `https://api.themoviedb.org/3/tv/${id}?api_key=${ApiKey}`
+    axios.get(url).then((val) => {
+        res.json(val.data)
+    }).catch((err) => {
+        res.send(err);
+    })
+}
+
+function defaultHandler(req, res) {
     const temp = {
         status: 404,
         responseText: "page not found"
     }
-    res.status(404).json(temp);
-})
+    res.status(404).send(temp)
+}
+function errorHandler(erorr, req, res) {
+    const err = {
+        status: 500,
+        massage: erorr
+    }
+    res.status(500).send(err);
+}
+
 server.listen(PORT, () => {
-    console.log('Server listening on port 3000');
+    console.log(`Server listening on port ${PORT}`);
 })
